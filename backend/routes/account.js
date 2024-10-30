081cbce7d9e5c208ff4d1192389bbe7a2b1943f1
YukichiOhno <juuzousuzuya200@gmail.com>
2024-10-30 09:59:13 -0500

    Update account.js
    Added a route for signing up employees

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -91,8 +91,103 @@ router.post('/sign-up/guide', async (req, res) => {
             error: err
         });
     }
-
 });
+
+
+router.post('/sign-up/employee', async (req, res) => {
+    // initialize variables
+    const accountInformation = req.body.account;
+    const employeeInformation = req.body.employee;
+    const employeeInitial = employeeInformation.initial ? employeeInformation.initial.toLowerCase() : null;
+    const transactionQuery = [];
+    let transactionResult;
+    let accountNumber;
+    let employeeNumber;
+    let hashedPassword;
+
+    // validate account information
+    if (!accountInformation.username || !accountInformation.password) {
+        logger.debug('missing username and password');
+        return res.status(400).json({ message: 'missing username and password' })
+    }
+
+    // validate account information
+    if (
+        !employeeInformation.first_name ||
+        !employeeInformation.last_name ||
+        !employeeInformation.dob ||
+        !employeeInformation.phone_number ||
+        !employeeInformation.email ||
+        !employeeInformation.address ||
+        !employeeInformation.city ||
+        !employeeInformation.state ||
+        !employeeInformation.zip ||
+        !employeeInformation.status ||
+        !employeeInformation.start_date ||
+        !employeeInformation.role 
+    ) {
+        logger.debug('required employee information are missing');
+        return res.status(400).json({ message: 'required employee information are missing'});
+    }
+
+    // hash the employee's password at saltround 10
+    hashedPassword = await bcrypt.hash(accountInformation.password, 10);
+    logger.debug("successfully hashed the employee's password");
+    delete accountInformation.password;
+
+    // generate an account number
+    accountNumber = generateCustomNumber('ACCT').toUpperCase();
+    logger.debug(`sucessfully generated an account number for the employee: ${accountNumber}`);
+
+    // generate an employee number
+    employeeNumber = generateCustomNumber('EMP').toUpperCase();
+    logger.debug(`sucessfully generated a guide number for the employee: ${employeeNumber}`);
+
+    try {
+        // insert statement to account table
+        transactionQuery.push({
+            query: "INSERT INTO account (account_number, account_username, account_password, account_identity) VALUES (?, ?, ?, ?)",
+            params: [accountNumber, accountInformation.username.toLowerCase(), hashedPassword, 'employee']
+        });
+
+        // insert statement to employee table
+        transactionQuery.push({
+            query: "INSERT INTO employee (employee_number, employee_first_name, employee_last_name, employee_initial, employee_dob, employee_phone_number, employee_email, employee_address, employee_city, employee_state, employee_zip, employee_status, employee_start_date, employee_role, account_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, LAST_INSERT_ID())",
+            params: [
+                employeeNumber,
+                employeeInformation.first_name.toLowerCase(),
+                employeeInformation.last_name.toLowerCase(),
+                employeeInitial,
+                employeeInformation.dob,
+                employeeInformation.phone_number,
+                employeeInformation.email.toLowerCase(),
+                employeeInformation.address.toLowerCase(),
+                employeeInformation.city.toLowerCase(),
+                employeeInformation.state.toUpperCase(),
+                employeeInformation.zip,
+                employeeInformation.status.toLowerCase(),
+                employeeInformation.start_date,
+                employeeInformation.role.toLowerCase()
+            ]
+        });
+
+        // execute transaction
+        transactionResult = await executeTransaction(transactionQuery);
+        logger.debug('sign-up success');
+        logger.debug('transaction result');
+        logger.debug(transactionResult);
+        return res.status(201).json({ message: "employee account successfully created", username: accountInformation.username.toLowerCase()});
+
+    } catch (err) {
+        logger.error('an error occured during employee sign-up transaction');
+        logger.error(err);
+        return res.status(500).json({ 
+            message: 'an error occured during employee sign-up transaction',
+            error: err
+        });
+    }
+});
+
 
 router.get('/', async (req, res) => {
     let selectQuery;
